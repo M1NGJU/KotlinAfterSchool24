21b18f72966d90c41eda6622661015aa8602af3b
M1NGJU <s2303@e-mirim.hs.kr>
2024-04-19 22:18:07 +0900

    문자열 예제

---
+++ src/TestStr.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val num = 10
+    val formatedString = """
+        var a = 6
+        var b = "Kotlin"
+         println(a + num)
+         """
+
+    println(formatedString)
+    val a = 1
+    val str1  = "a = $a"
+    val str2 = "a = $(a+2)"
+    println("str1: \" $str1\", str2:\"$str2\"")
+}
