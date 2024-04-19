ad41b535633d70982b61c81e8447a1056b45b0ca
M1NGJU <s2303@e-mirim.hs.kr>
2024-04-19 22:11:29 +0900

    기본 문법

---
+++ src/Test01.kt
@@ -0,0 +1,6 @@
+fun main() {
+    var name:String = "Kotiln"
+    val age:Int=20
+    println("이름= $name")
+    println("나이= ${age+1}")
+}
---
+++ src/Test4.kt
@@ -0,0 +1,7 @@
+//fun main() {
+//    var str = "Hello"
+//    var result = if(str != null) str.length else 0
+//    print("len = ${len}")
+//    var len2 = str?.length?:0
+//    print("len2 = ${len2}")
+//}
